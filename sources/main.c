cda299c0821885dea943e4453634ca0b544bd817
jiwoo181155 <jwj9909@gmail.com>
2023-12-18 14:20:37 +0900

    메뉴 인터페이스 개선화&일부 코드 수정

--- sources/main.c
+++ sources/main.c
@@ -23,37 +23,85 @@ int main() {
 
   while (1) {
     int choice = -1;
+    int choice_employee = -1;
+    int choice_resigned = -1;
+    int roof_employee = -1;
+    int roof_resigned = -1;
     printf("-------------------------------\n");
-    printf("원하는 메뉴를 입력해주세요.\n");
-    printf("1.사원 명단 신규 추가\n2.사원 명단 수정\n3.사원 명단 보기\n");
-    printf("4.퇴사자 명단 이동\n5.퇴사자 명단 보기\n");
-    printf("6.휴가자 명단 추가\n7.휴가자 명단 보기\n8.남은 연차 보기\n");
-    printf("9.종료\n");
+    printf("         <Main Menu>\n");
+    printf("■원하는 메뉴를 입력해주세요.■\n");
+    printf("-------------------------------\n");
+    printf("1.사원 관리\n2.퇴사 관리\n3.휴가 관리\n4.종료\n");
     printf("-------------------------------\n");
     printf("메뉴: ");
     scanf_s("%d", &choice);
-
+    printf("-------------------------------\n");
     switch (choice) {
       case 1:
-        AddEmployee(EmployeeTable, &num_people);
+        while (roof_employee) {
+          printf("-------------------------------\n");
+          printf("         <사원 메뉴>\n");
+          printf("■원하는 메뉴를 입력해주세요.■\n");
+          printf("-------------------------------\n");
+          printf(
+              "1.사원 명단 신규 추가\n2.사원 명단 수정\n3.사원 명단 보기\n4.메인메뉴로 돌아가기\n");
+          printf("-------------------------------\n");
+          printf("메뉴: ");
+          scanf_s("%d", &choice_employee);
+          printf("-------------------------------\n");
+          switch (choice_employee) {
+            case 1:
+              AddEmployee(EmployeeTable, &num_people);
+              break;
+            case 2:
+              UpdateEmployee(EmployeeTable, num_people);
+              break;
+            case 3:
+              PrintEmployee(EmployeeTable, num_people);
+              break;
+            case 4:
+              roof_employee = 0;
+              break;
+            default:
+              printf("잘못된 메뉴 선택입니다.\n");
+          }
+        }
         break;
       case 2:
-        UpdateEmployee(EmployeeTable, num_people);
+        while (roof_resigned) {
+          printf("-------------------------------\n");
+          printf("         <퇴사 메뉴>\n");
+          printf("■원하는 메뉴를 입력해주세요.■\n");
+          printf("-------------------------------\n");
+          printf("1.퇴사자 명단 이동\n2.퇴사자 명단 보기\n3.메인메뉴로 돌아가기\n");
+          printf("-------------------------------\n");
+          printf("메뉴: ");
+          scanf_s("%d", &choice_resigned);
+          printf("-------------------------------\n");
+          switch (choice_resigned) {
+            case 1:
+              MoveToResigned(EmployeeTable, &num_people);
+              break;
+            case 2:
+              PrintResigned(ResignedTable, num_resigned);
+              break;
+            case 3:
+              roof_resigned = 0;
+              break;
+            default:
+              printf("잘못된 메뉴 선택입니다.\n");
+          }
+        }
         break;
       case 3:
-        PrintEmployee(EmployeeTable, num_people);
+
         break;
       case 4:
-        MoveToResigned(EmployeeTable, &num_people);
-        break;
-      case 5:
-        PrintResigned(ResignedTable, num_resigned);
-        break;
-      case 9:
         FreeMemory_Employee();
         FreeMemory_ResignedEmployee();
         printf("프로그램을 종료합니다.");
         return 0;  // 종료
+        break;
       default:
         printf("잘못된 메뉴 선택입니다.\n");
     }
